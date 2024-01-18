a0163856b4d203514c74fcb10705687c75f8711f
Vishal Ghosh <avatarvishal1551@gmail.com>
2024-01-19 00:36:55 +0530

    editing login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:chatapp/pages/loginPage.dart';
+import 'package:chatapp/themes/light_Mode.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,6 +13,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Minimal Chat App',
+      debugShowCheckedModeBanner: false,
+      theme: lightMode,
       home: LoginPage(),
     );
   }
--- lib/pages/loginPage.dart
+++ lib/pages/loginPage.dart
@@ -1,15 +1,72 @@
+import 'package:chatapp/components/TextField_Editor.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+  LoginPage({super.key});
 
   @override
   State<LoginPage> createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //! LOGO
+
+            Icon(
+              Icons.message,
+              size: 60,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+
+            const SizedBox(
+              height: 20,
+            ),
+
+            //! TEXT
+
+            Text(
+              "Welcome Back",
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.primary,
+                fontSize: 16,
+              ),
+            ),
+
+            const SizedBox(
+              height: 20,
+            ),
+
+            //* EMAIL TEXT FIELD
+
+            CustomTextField(
+              hintText: "Email",
+              obscureText: false,
+              controller: emailController,
+            ),
+
+            const SizedBox(
+              height: 20,
+            ),
+
+            //* PW TEXT FIELD
+            CustomTextField(
+              hintText: "Password",
+              obscureText: true,
+              controller: passwordController,
+            )
+            //* REGISTRATION
+          ],
+        ),
+      ),
+    );
   }
 }
